f2739024c12e8fe007e41f45807f232c714e8425
Samadi van Koten <samadi@vktec.org.uk>
2021-07-05 01:43:16 +0100

    Rework installation directory and add package dependency resolution

--- Deps.zig
+++ Deps.zig
@@ -1,78 +1,77 @@
+// Possible TODOs:
+// - Parse source to ensure all dependencies are actually used
+
 const std = @import("std");
 
 b: *std.build.Builder,
-deps: std.ArrayListUnmanaged(Dep) = .{},
+deps: std.StringArrayHashMapUnmanaged(Dep) = .{},
+dir: []const u8,
+import_set: std.StringArrayHashMapUnmanaged(void) = .{},
 
 const Deps = @This();
 pub const Dep = struct {
-    name: []const u8, // Name of package
-    path: []const u8, // Path to package main file, relative to pwd
+    path: []const u8, // Absolute path to package main file
+    deps: []const []const u8, // Dependencies of this package
 };
 
 pub fn init(b: *std.build.Builder) Deps {
-    return .{ .b = b };
+    const dir = switch (std.builtin.os.tag) {
+        .windows => b.fmt("{s}\\Temp\\deps-zig\\", .{std.os.getenv("LOCALAPPDATA").?}),
+        .macos => b.fmt("{s}/Library/Caches/deps-zig/", .{std.os.getenv("HOME").?}),
+        else => if (std.os.getenv("XDG_CACHE_HOME")) |cache|
+            b.fmt("{s}/deps-zig/", .{cache})
+        else
+            b.fmt("{s}/.cache/deps-zig/", .{std.os.getenv("HOME").?}),
+    };
+
+    std.fs.makeDirAbsolute(dir) catch {};
+    var dirh = std.fs.openDirAbsolute(dir, .{}) catch |err| {
+        std.debug.print("Could not open packages dir '{}': {s}\n", .{ std.fmt.fmtSliceEscapeLower(dir), @errorName(err) });
+        std.os.exit(1);
+    };
+    defer dirh.close();
+    // Purposefully leak the file descriptor - it will be unlocked when the process exits
+    _ = dirh.createFile(".lock", .{ .lock = .Exclusive, .lock_nonblocking = true }) catch |err| {
+        std.debug.print("Failed to aqcuire package lock: {s}\n", .{@errorName(err)});
+        std.os.exit(1);
+    };
+
+    return .{ .b = b, .dir = dir };
+}
+
+pub fn addTo(self: Deps, step: *std.build.LibExeObjStep) void {
+    var it = self.deps.iterator();
+    while (it.next()) |entry| {
+        step.addPackage(self.createPkg(entry.key_ptr.*, entry.value_ptr.*));
+    }
+}
+fn createPkg(self: Deps, name: []const u8, dep: Dep) std.build.Pkg {
+    return .{
+        .name = name,
+        .path = .{ .path = dep.path },
+        .dependencies = if (dep.deps.len == 0) null else blk: {
+            const deps = self.b.allocator.alloc(std.build.Pkg, dep.deps.len) catch unreachable;
+            var i: usize = 0;
+            for (dep.deps) |dname| {
+                if (self.deps.get(dname)) |ddep| {
+                    deps[i] = self.createPkg(dname, ddep);
+                    i += 1;
+                }
+                // If we don't have the dep, ignore it and let the compiler error
+            }
+            break :blk deps[0..i];
+        },
+    };
 }
 
 pub fn add(self: *Deps, url: []const u8, version: []const u8) void {
-    const base_dir = std.fs.path.dirname(@src().file) orelse ".";
     const url_base = std.fs.path.basenamePosix(url);
     const name = trimPrefix(u8, trimSuffix(u8, url_base, ".git"), "zig-");
-    const dir = std.fs.path.join(self.b.allocator, &.{ base_dir, name }) catch unreachable;
-
-    std.fs.cwd().access(dir, .{}) catch {
-        // Check if we're in a git repo
-        if (self.execOk(&.{ "git", "rev-parse", "--is-inside-work-tree" }, base_dir)) {
-            // We are, so use submodules
-            self.exec(&.{
-                "git",
-                "submodule",
-                "add",
-                "--depth=1",
-                "--",
-                url,
-                name,
-            }, base_dir);
-        } else {
-            // We aren't, so clone
-            self.exec(&.{
-                "git",
-                "clone",
-                "--depth=1",
-                "--no-single-branch",
-                "--shallow-submodules",
-                "--",
-                url,
-                name,
-            }, base_dir);
-        }
-    };
-
-    self.exec(&.{ "git", "fetch", "--all", "-Ppqt" }, dir);
-    // Check if there are changes - we don't want to clobber them
-    if (self.execOk(&.{ "git", "diff", "--quiet", "HEAD" }, dir)) {
-        // Clean; check if version is a branch
-        if (self.execOk(&.{
-            "git",
-            "show-ref",
-            "--verify",
-            "--",
-            self.b.fmt("refs/heads/{s}", .{version}),
-        }, dir)) {
-            // It is, so switch to it and pull
-            self.exec(&.{ "git", "switch", "-q", "--", version }, dir);
-            self.exec(&.{ "git", "pull", "-q", "--ff-only" }, dir);
-        } else {
-            // It isn't, check out detached
-            self.exec(&.{ "git", "switch", "-dq", "--", version }, dir);
-        }
-    } else {
-        // Dirty; print a warning
-        std.debug.print("WARNING: package {s} contains uncommitted changes, not attempting to update\n", .{name});
-    }
+    const path = self.fetchPkg(name, url, version);
 
     const main_file = blk: {
-        var dirh = std.fs.cwd().openDir(dir, .{}) catch {
-            std.debug.print("Failed to open package dir: {s}\n", .{dir});
+        var dirh = std.fs.cwd().openDir(path, .{}) catch {
+            std.debug.print("Failed to open package dir: {s}\n", .{path});
             std.os.exit(1);
         };
         for ([_][]const u8{
@@ -91,12 +90,157 @@ pub fn add(self: *Deps, url: []const u8, version: []const u8) void {
         std.debug.print("Could not find package entrypoint, attempted {s}.zig, main.zig, src{c}{[0]s}.zig and src{[1]c}main.zig\n", .{ name, std.fs.path.sep });
         std.os.exit(1);
     };
-    const path = std.fs.path.join(self.b.allocator, &.{ dir, main_file }) catch unreachable;
+    const main_path = std.fs.path.join(self.b.allocator, &.{ path, main_file }) catch unreachable;
 
-    self.deps.append(self.b.allocator, .{
-        .name = name,
-        .path = path,
-    }) catch unreachable;
+    const deps = self.parsePackageDeps(main_path) catch |err| switch (err) {
+        error.InvalidSyntax => &[_][]const u8{},
+        else => {
+            std.debug.print("Failed to parse package dependencies for {s}: {s}\n", .{ main_file, @errorName(err) });
+            std.os.exit(1);
+        },
+    };
+
+    if (self.deps.fetchPut(self.b.allocator, name, .{ .path = main_path, .deps = deps }) catch unreachable) |_| {
+        std.debug.print("Duplicate dependency '{s}'\n", .{std.fmt.fmtSliceEscapeLower(name)});
+        std.os.exit(1);
+    }
+}
+
+fn fetchPkg(self: Deps, name: []const u8, url: []const u8, version: []const u8) []const u8 {
+    const path = self.b.allocator.alloc(u8, self.dir.len + url.len + 1 + version.len) catch unreachable;
+
+    // Base dir (includes path sep)
+    var i: usize = 0;
+    std.debug.assert(self.dir[self.dir.len - 1] == std.fs.path.sep);
+    std.mem.copy(u8, path[i..], self.dir);
+    i += self.dir.len;
+
+    // Encoded URL (/ replaced with : so it's a valid path)
+    std.mem.copy(u8, path[i..], url);
+    std.mem.replaceScalar(u8, path[i .. i + url.len], '/', ':');
+    i += url.len;
+
+    // Version separator
+    path[i] = '@';
+    i += 1;
+
+    // Version
+    std.mem.copy(u8, path[i..], version);
+    i += version.len;
+    std.debug.assert(i == path.len);
+
+    // If we don't have the dep already, clone it
+    std.fs.cwd().access(path, .{}) catch {
+        self.exec(&.{
+            "git",
+            "clone",
+            "--depth=1",
+            "--no-single-branch",
+            "--shallow-submodules",
+            "--",
+            url,
+            path,
+        }, null);
+    };
+
+    self.exec(&.{ "git", "fetch", "--all", "-Ppqt" }, path);
+    // Check if there are changes - we don't want to clobber them
+    if (self.execOk(&.{ "git", "diff", "--quiet", "HEAD" }, path)) {
+        // Clean; check if version is a branch
+        if (self.execOk(&.{
+            "git",
+            "show-ref",
+            "--verify",
+            "--",
+            self.b.fmt("refs/heads/{s}", .{version}),
+        }, path)) {
+            // It is, so switch to it and pull
+            self.exec(&.{ "git", "switch", "-q", "--", version }, path);
+            self.exec(&.{ "git", "pull", "-q", "--ff-only" }, path);
+        } else {
+            // It isn't, check out detached
+            self.exec(&.{ "git", "switch", "-dq", "--", version }, path);
+        }
+    } else {
+        // Dirty; print a warning
+        std.debug.print("WARNING: package {s} contains uncommitted changes, not attempting to update\n", .{name});
+    }
+
+    return path;
+}
+
+fn parsePackageDeps(self: *Deps, main_file: []const u8) ![]const []const u8 {
+    defer self.import_set.clearRetainingCapacity();
+
+    var npkg = try self.collectImports(std.fs.cwd(), main_file);
+    const pkgs = try self.b.allocator.alloc([]const u8, npkg);
+    for (self.import_set.keys()) |key| {
+        if (isPkg(key)) {
+            npkg -= 1;
+            pkgs[npkg] = key;
+        }
+    }
+
+    return pkgs;
+}
+fn collectImports(self: *Deps, dir: std.fs.Dir, import: []const u8) CollectImportsError!usize {
+    const data = dir.readFileAlloc(self.b.allocator, import, 4 << 30) catch |err| switch (err) {
+        error.FileTooBig => {
+            // If you have a 4GiB source file, you have a problem
+            // However, we probably shouldn't outright error in this situation, so instead we'll warn and skip this file
+            std.debug.print("Could not parse exceptionally large source file '{s}', skipping\n", .{std.fmt.fmtSliceEscapeLower(import)});
+            return 0;
+        },
+        else => |e| return e,
+    };
+    var subdir = try dir.openDir(std.fs.path.dirname(import) orelse ".", .{});
+    defer subdir.close();
+
+    var toks = std.zig.Tokenizer.init(data);
+    var npkg: usize = 0;
+    while (true) {
+        const tok = toks.next();
+        if (tok.tag == .eof) break;
+
+        if (tok.tag == .builtin and std.mem.eql(u8, data[tok.loc.start..tok.loc.end], "@import")) {
+            if (toks.next().tag != .l_paren) return error.InvalidSyntax;
+            const name_tok = toks.next();
+            if (name_tok.tag != .string_literal) return error.InvalidSyntax;
+            if (toks.next().tag != .r_paren) return error.InvalidSyntax;
+
+            const name = std.zig.string_literal.parseAlloc(
+                self.b.allocator,
+                data[name_tok.loc.start..name_tok.loc.end],
+            ) catch |err| switch (err) {
+                error.InvalidStringLiteral => return error.InvalidSyntax,
+                else => |e| return e,
+            };
+
+            if (try self.import_set.fetchPut(self.b.allocator, name, {})) |_| {
+                // Do nothing, the entry is already in the set
+            } else if (isPkg(name)) {
+                npkg += 1;
+            } else if (std.mem.endsWith(u8, name, ".zig")) {
+                npkg += try self.collectImports(subdir, name);
+            }
+        }
+    }
+
+    return npkg;
+}
+const CollectImportsError =
+    std.fs.Dir.OpenError ||
+    std.fs.File.OpenError ||
+    std.fs.File.ReadError ||
+    std.fs.File.SeekError ||
+    std.mem.Allocator.Error ||
+    error{InvalidSyntax};
+
+fn isPkg(name: []const u8) bool {
+    if (std.mem.endsWith(u8, name, ".zig")) return false;
+    if (std.mem.eql(u8, name, "std")) return false;
+    if (std.mem.eql(u8, name, "root")) return false;
+    return true;
 }
 
 fn trimPrefix(comptime T: type, haystack: []const T, needle: []const T) []const T {
@@ -152,10 +296,4 @@ fn execInternal(self: Deps, argv: []const []const u8, cwd: ?[]const u8, io: std.
         },
     }
     return true;
-}
-
-pub fn addTo(self: Deps, step: *std.build.LibExeObjStep) void {
-    for (self.deps.items) |dep| {
-        step.addPackagePath(dep.name, dep.path);
-    }
 }
--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 # Deps.zig
 
-Deps.zig is a dependency manager for Zig. It does not work like a typical package manager,
-but instead provides a simple way to interact with git submodules from a `build.zig`.
-It is designed for simplicity from both an implementation and usability standpoint. The API
-is similar to the other APIs used in `build.zig`, and is implemented in under 150 lines of code.
+Deps.zig is a dependency manager for Zig. It is designed for simplicity from both an implementation
+and usability standpoint. The API is similar to the other APIs used in `build.zig`, and is implemented
+in around 300 lines of code.
+
+It is configured solely through `build.zig`, and automatically builds dependency trees without any extra
+metadata, however it requires every package in the tree to be registered manually. This means you may need
+to specify packages you don't directly depend on, but allows for more flexibility as you can swap out
+compatible implementations as you see fit.
 
 ## Usage
 
-Deps.zig is a ~150 line, standalone Zig file that is designed to be copied into your project.
-Since it will install dependencies into the same directory it is located in, it is recommended
-to place it in a directory named `deps`.
+Deps.zig is a standalone Zig file that is designed to be copied into your project.
+It will install packages into a system-specific directory:
 
-Not only is this a simple and descriptive name for a directory that holds dependencies, it is
-also one that is whitelisted by GitHub's [Linguist], meaning code placed within it will not be
-counted in your repo's language breakdown.
+- *nix: `$XDG_CACHE_HOME/deps-zig/`
+- Windows: `%LOCALAPPDATA%\Temp\deps-zig\`
+- macOS: `~/Library/Caches/deps-zig/`
 
 To use Deps.zig in your `build.zig`, simply add code like the following, or look in the `example`
 directory in this repo for a more complete example.
 
 ```zig
 // Create a new dependency list
-var deps = @import("deps/Deps.zig").init(b);
+var deps = @import("Deps.zig").init(b);
 // Add a package, with a "version" of `main`, meaning it will use the latest commit on the `main` branch
 deps.add("https://github.com/vktec/zig-uuid", "main");
 // Add all registered packages to the executable step
 deps.addTo(exe);
 ```
-
-[Linguist]: https://github.com/github/linguist
